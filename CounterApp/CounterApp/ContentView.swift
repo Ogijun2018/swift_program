19cd887c54dde4c25da6f51e710391fee680d5a5
Ogijun2018 <o2030028@edu.cc.uec.ac.jp>
2020-12-03 16:50:24 +0900

    クロージャ完了

--- CounterApp/CounterApp/ContentView.swift
+++ CounterApp/CounterApp/ContentView.swift
@@ -11,12 +11,23 @@ struct ContentView: View {
     @State var number = 0
     var body: some View {
         VStack {
-            Text("\(number)")
-                .padding()
-//            クロージャ
-            Button(action: {self.number += 1}) {
-                Text("カウント")
+            ZStack{
+                Image("counter")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Text("\(number)")
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                    .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                    .padding()
             }
+//            クロージャ
+            Button(action: {self.number += 1}, label: {Text("カウント")
+                    .foregroundColor(.white)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+                    .cornerRadius(10.0)
+                }
+            )
 //            こうもかける
 //            Button(action: {() -> Void in self.number += 1}){
 //                Text("カウント")
